53fdc80a5aa02e238e1fdd2dc08b5135517bd656
Niima <nima.h@live.com>
2016-03-19 14:23:42 +0330

    replace  {R:n} with

--- convert.js
+++ convert.js
@@ -4,10 +4,21 @@ function convert(){
     xmlDoc = $.parseXML( xml ),
     $xml = $( xmlDoc );
     $xml.find("rule").each(function(){
-        $('#result').append(
+        
+        var str2   = $(this).find("rule>action").attr('url');
+        var regex = /{R:(\d{1})}/;
+        while (regex.test(str2)) {
+		  str2 = str2.replace(regex, '\$'+ '$1' );
+	    }  
+        // if (str2.match(regex)) {
+        //     str2 = str2.replace(regex, "$1" + "nima");
+        // }
+
+        
+        $('#result').append( "RewriteRule " +
             $(this).find("rule>match").attr('url') +
-                " &nbsp;&nbsp;&nbsp; " + 
-                $(this).find("rule>action").attr('url') +
+                " &nbsp;&nbsp; " + 
+                 str2 + 
                 "<br>");
     });
 	
