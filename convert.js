7350a6b774ae6f9899a500ba865beed8269ea104
Niima <nima.h@live.com>
2016-03-19 17:26:15 +0330

    final touch

--- convert.js
+++ convert.js
@@ -4,17 +4,11 @@ function convert(){
     xmlDoc = $.parseXML( xml ),
     $xml = $( xmlDoc );
     $xml.find("rule").each(function(){
-        
         var str2   = $(this).find("rule>action").attr('url');
         var regex = /{R:(\d{1})}/;
         while(regex.test(str2)){ 
                 str2 = str2.replace(regex, '&#36;' + RegExp.$1 );
-             
         }
-        // if (str2.match(regex)) {
-        //     str2 = str2.replace(regex, "$1" + "nima");
-        // }
-
         
         $('#result').append( "RewriteRule " +
             $(this).find("rule>match").attr('url') +
